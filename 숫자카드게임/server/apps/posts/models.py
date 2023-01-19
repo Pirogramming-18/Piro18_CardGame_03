98d12c8c8eca0a7ab6fdaf14f936ab5c5d29d44e
Chaewon <ksdyys3@naver.com>
2023-01-19 15:58:46 +0900

    모델 추가

--- 숫자카드게임/server/apps/posts/models.py
+++ 숫자카드게임/server/apps/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=32)
+    #password = models.CharField(max_length=32)
+    #email = models.CharField(max_length=64)
+    scoreAll = models.IntegerField()
+
+class Game(models.Model):
+    hostUser= models.ForeignKey(User, on_delete=models.CASCADE)
+    guestUser= models.ForeignKey(User, on_delete=models.CASCADE)
+    result = models.CharField(max_length=8)
+    winner = models.CharField(max_length=32)
+    ing=models.BooleanField()
+    standard=models.BooleanField() #높은쪽을 True
+    score = models.IntegerField()
