d55de7a15768b3ff26fd6a2c26c5e758b353408c
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-21 05:08:11 +0900

    게임 html,css view, url 합치기

--- 숫자카드게임/server/apps/posts/urls.py
+++ 숫자카드게임/server/apps/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from server.apps.posts.views import main
+from server.apps.posts.views import main, GameCreate, counterattack, gameStart
 from . import views
+
 
 app_name='posts' #네임 스페이스
 urlpatterns= [
@@ -11,5 +12,10 @@ urlpatterns= [
     path('signup/', views.signup, name='signup'),
     path("posts/gamelist", views.game_list, name='game_list'),
     path("posts/lanking", views.lanking, name='game_lanking'),
-    path('posts/gameinfo/<int:pk>', views.game_info, name='game_info')
+    path('posts/gameinfo/<int:pk>', views.game_info, name='game_info'),
+    path("gameStatus/<int:pk>", gameStart),
+    path("attack/<str:uid>",  GameCreate, name="attack"),
+    path("counterattack/<int:pk>", counterattack, name="counterattack"),
+
+    
 ]
--- 숫자카드게임/server/apps/posts/views.py
+++ 숫자카드게임/server/apps/posts/views.py
@@ -5,6 +5,8 @@ from .models import User,GestUser,Game
 from django.http.request import HttpRequest
 from django.db.models import Q
 
+from random import randint
+
 def login(request:HttpRequest,*args, **kwargs):
     hostUser='1' #호스트 유저 아이디 받아오는곳!
     return(hostUser)
@@ -54,3 +56,122 @@ def game_info(request, pk, *args, **kwargs) :
         "user":user,
     }
     return render(request, "posts/game_info.html", context=context )
+
+
+    #make criteria for winning the game
+def gameStart(request, pk, *args, **kwargs):
+    # criteriaList = [True, False, 3 , 7]
+    criteriaList = [True, False]
+    idx = randint(0, 1)
+    game = Game.objects.get(id = pk)
+    hostUser = game.hostUser
+    guestUser = game.guestUser
+    hostCard = game.hostUser.card
+    guestCard = game.guestUser.card
+    rule = "큰" if criteriaList[idx] else "작은"
+    context = {
+        "hostUser" : hostUser,
+        "guestUser" : guestUser,
+        "game" : game,
+        "rule" : rule,
+    }
+    
+    
+    if idx == 0:
+        game.result = "승리" if hostCard > guestCard else "패배"
+    elif idx == 1:
+        game.result = "승리" if hostCard < guestCard else "패배"
+    #criteria for winning is close to three
+    # elif idx == 2:
+    #   game.result = "승리" if abs(hostCard - 3) < abs(guestCard - 3) else "패배"
+    # elif idx == 3:
+    #   game.result = "승리" if abs(hostCard - 7) < abs(guestCard - 7) else "패배"
+        
+    game.winner = game.hostUser if game.result =="승리" else game.guestUser
+    
+    if game.result == "승리":
+        game.hostUser.scoreAll += game.hostUser.card
+        game.guestUser.scoreAll -= game.hostUser.card
+    else:
+        game.hostUser.scoreAll -= game.hostUser.card
+        game.hostUser.scoreAll += game.hostUser.card
+    
+    return render(request, "game/gameStatus.html", context=context)
+
+        
+def GameCreate(request: HttpRequest, uid, *args, **kwargs):
+    
+    idxList = [0,1,2,3,4]
+    cardList = []
+    user =User.objects.get(user_id=uid)
+    users = User.objects.all().exclude(user_id=uid)
+    # print(users)
+    # print(user)
+    for i in range(5): #make five integers
+        card = randint(1, 10)
+        while card in cardList: #not for overlapping
+        card = randint(1, 10)
+        cardList.append(card)
+    # print(cardList)
+    
+    user.cardList = list(zip(idxList,cardList))
+    user.save()
+    # print(user.cardList)
+    context ={
+        "user" : user,
+        "users" : users,
+    }
+
+    if request.method == "POST":
+        # user.card = request.POST["card"]
+        # user.save()
+        Game.objects.create(
+        hostUser = user,
+        guestUser = User.objects.get(user_id =  request.POST.get("guestUser")),
+        result = "시작전",
+        winner = "",
+        ing = False,
+        standard = False,
+        score = 0,
+        accept = False,
+        )
+        game = Game.objects.get(hostUser=user, guestUser=User.objects.get(user_id =  request.POST.get("guestUser")))
+        print(game.guestUser.user_id)
+        return redirect(f"/counterattack/{game.id}",  context=context)
+
+    return render(request, "game/attack.html", context=context)
+
+
+
+def counterattack(request: HttpRequest, pk, *args, **kwargs):
+    
+    idxList = [0,1,2,3,4]
+    cardList = []
+    game = Game.objects.get(id=pk)
+    hostUser = game.hostUser
+    user = game.guestUser
+    print(game.guestUser.user_id)
+    for i in range(5): #make five integers
+        card = randint(1, 10)
+        while card in cardList: #not for overlapping
+            card = randint(1, 10)
+        cardList.append(card)
+    # print(cardList)
+    
+    user.cardList = list(zip(idxList,cardList))
+    user.save()
+    print(user.card)
+    # print(user.cardList)
+    context ={
+        "game" : game,
+        "user" : user,
+    }
+
+    if request.method == "POST":
+        print("hello")
+        game.accept=True
+        game.save()
+        return redirect(f"/counterattack/{game.id}",  context=context)
+        # return redirect(f"gameStatus/{game.id}",  context=context)
+
+    return render(request, "game/counterattack.html", context=context)
