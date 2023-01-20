8b3d8e3d6d76345e6fa7f15da1578b017893d6b2
Chaewon <ksdyys3@naver.com>
2023-01-20 21:34:46 +0900

    html,view 완성

--- 숫자카드게임/server/apps/posts/admin.py
+++ 숫자카드게임/server/apps/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User,GestUser,Game
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(User)
+admin.site.register(GestUser)
